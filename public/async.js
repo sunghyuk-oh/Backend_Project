fe9227ad0e702164036284b4aaa27dcc5852207d
Tim Oh <dhquffl@gmail.com>
2021-09-01 09:41:16 -0400

    comment/sort functionalities added

--- public/async.js
+++ public/async.js
@@ -2,10 +2,51 @@ const commentBox = document.getElementById('commentBox')
 const commentFormBox = document.getElementById('commentFormBox')
 const blogIdTextBox = document.getElementById('blogIdTextBox')
 const bodyTextBox = document.getElementById('bodyTextBox')
+const newestBtn = document.getElementById('newestBtn')
+const oldestBtn = document.getElementById('oldestBtn')
+const mainImageBox = document.getElementById('mainImageBox')
 
 
-function displayComment() {
-    fetch("http://localhost:8080/blog/leave-comment")
+function sortByNewest() {
+    fetch('http://localhost:8080/sort-by-newest')
+        .then(response => response.json())
+        .then(blogs => {
+            sortedBlogs = blogs.map((blog) => {
+                return `<div id="container">
+                            <div class="fill"><img src="${blog.image}" alt=""></div>
+                            <div id = "description">
+                                <a href="/details/${blog.id}">
+                                    <h3>${blog.title}</h3>
+                                </a>
+                                <p>${blog.createdAt}</p>
+                            </div>
+                        </div>`
+            })
+            mainImageBox.innerHTML = sortedBlogs.join('')
+        })
+}
+
+function sortByOldest() {
+    fetch('http://localhost:8080/sort-by-Oldest')
+        .then(response => response.json())
+        .then(blogs => {
+            sortedBlogs = blogs.map((blog) => {
+                return `<div id="container">
+                        <div class="fill"><img src="${blog.image}" alt=""></div>
+                        <div id = "description">
+                            <a href="/details/${blog.id}">
+                                <h3>${blog.title}</h3>
+                            </a>
+                            <p>${blog.createdAt}</p>
+                        </div>
+                    </div>`
+            })
+            mainImageBox.innerHTML = sortedBlogs.join('')
+        })
+}
+
+function displayComment(result) {
+    fetch(`http://localhost:8080/${result.oneComment.blog_id}/leave-comment`)
         .then(response => response.json())
         .then(comments => {
             let commentItems = comments.map((comment) => {
@@ -18,32 +59,30 @@ function displayComment() {
         .catch(error => {
             console.log(error)
         })
-
 }
 
-// function fetchComment(event) {
-//}
-
-commentFormBox.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log(bodyTextBox.value)
-    console.log(blogIdTextBox.value)
-    fetch("http://localhost:8080/blog/leave-comment", {
+function postComment() {
+    fetch("http://localhost:8080/leave-comment", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                body_text: bodyTextBox.value,
-                blog_id: blogIdTextBox.value
+                bodyText: bodyTextBox.value,
+                blogId: blogIdTextBox.value
             })
         })
         .then(response => response.json())
         .then(result => {
             console.log(result)
-            displayComment()
+            displayComment(result)
         })
         .catch(error => {
             console.log(error)
         })
+}
+
+commentFormBox.addEventListener('submit', (e) => {
+    e.preventDefault()
+    postComment()
 })
--- routers/blog.js
+++ routers/blog.js
@@ -25,22 +25,5 @@ router.post('/post-blog', async(req, res) => {
     res.redirect('/blog')
 })
 
-// Leave a comment
-router.get('/leave-comment', async(req, res) => {
-    const blogs_by_id = await models.Comment.findAll({})
-
-    res.json(blogs_by_id)
-})
-
-router.post('/leave-comment', async(req, res) => {
-    const { bodyText, blogId } = req.body
-
-    const comment = await models.Comment.create({
-        body_text: bodyText,
-        blog_id: blogId
-    })
-
-    res.json({ success: true })
-})
 
 module.exports = router
